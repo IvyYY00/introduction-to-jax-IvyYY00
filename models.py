6d95c3fa103ce73d51fa67119a2c9900ce8e11f1
IvyYY00 <ivyyouy@gmail.com>
2024-10-03 20:35:55 -0400

    to be continued

--- models.py
+++ models.py
@@ -8,7 +8,16 @@ class CNNEmulator(eqx.Module):
     layers: list
 
     def __init__(self, key: PRNGKeyArray, hidden_dim: Int = 4):
-        raise NotImplementedError
+        layer = []
+        layer.append(eqx.nn.Conv(2,hidden_dim,1),key = key)
+        layer.append(eqx.nn.Lambda(jax.nn.tanh))
+        for i in range(4):
+            key,subkey = jax.random.split(key)
+            layers.append(eqx.nn.Conv2d(hidden_dim, hidden_dim, 3, padding=[1,1], key=subkey))
+
+        
 
     def __call__(self, x: Float[Array, "2 n_res n_res"]) -> Float[Array, "1 n_res n_res"]:
-        raise NotImplementedError
+        for layer in self.layers:
+            x = layer()
+        return x
